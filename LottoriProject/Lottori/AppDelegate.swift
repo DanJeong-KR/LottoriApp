654a120c4fa6ac5777f57bee45c4f86e8955b441
changSic <a920506@gmail.com>
2019-09-04 02:21:38 +0900

    feat : connect firebase

--- LottoriProject/Lottori/AppDelegate.swift
+++ LottoriProject/Lottori/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,14 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+      
+      // for first screen
         Thread.sleep(forTimeInterval: 1.3)
-        
+      
+      // init rootVC
         window = UIWindow(frame: UIScreen.main.bounds)
-        
         window?.rootViewController = UINavigationController(rootViewController: MainViewController())
-        
         window?.makeKeyAndVisible()
+      
+      // firebase
+      FirebaseApp.configure()
         
         return true
     }
